01697a0e505da233634a994f9328feb810be24fb
Stoyan Delev <mutebg@gmail.com>
2025-01-22 15:21:55 +0200

    add more sport

--- RunRideStudioWidget/Source/Base/Entity/AppIntentSportType.swift
+++ RunRideStudioWidget/Source/Base/Entity/AppIntentSportType.swift
@@ -8,8 +8,8 @@
 import AppIntents
 
 enum AppIntentSportType: String, AppEnum {
-    case run, ride, swim, walk, hike, rowing
-
+    case run, weighttraining, ride, walk, swim, hike, alpineski, badminton, backcountryski, canoeing, crossfit, elliptical, golf, iceskate, inlineskate, handcycle, highintensityintervaltraining, kayaking, kitesurf, nordicski, pickleball, pilates, racquetball, rockclimbing, rollerski, rowing, sail, skateboard, snowboard, snowshoe, soccer, squash, standuppaddling, stairstepper, surfing, tabletennis, tennis, velomobile, windsurf, wheelchair, workout, yoga
+    
     static var typeDisplayRepresentation: TypeDisplayRepresentation = "Sports"
     static var caseDisplayRepresentations: [AppIntentSportType : DisplayRepresentation] = [
         .run: "Run",
@@ -18,6 +18,43 @@ enum AppIntentSportType: String, AppEnum {
         .walk: "Walk",
         .hike: "Hike",
         .rowing: "Rowing",
+        .weighttraining: "WeightTraining",
+        .alpineski: "AlpineSki",
+        .badminton: "Badminton",
+        .backcountryski: "BackcountrySki",
+        .canoeing: "Canoeing",
+        .crossfit: "Crossfit",
+        .elliptical: "Elliptical",
+        .golf: "Golf",
+        .iceskate: "IceSkate",
+        .inlineskate: "InlineSkate",
+        .handcycle: "Handcycle",
+        .highintensityintervaltraining: "HighIntensityIntervalTraining",
+        .kayaking: "Kayaking",
+        .kitesurf: "Kitesurf",
+        .nordicski: "NordicSki",
+        .pickleball: "Pickleball",
+        .pilates: "Pilates",
+        .racquetball: "Racquetball",
+        .rockclimbing: "RockClimbing",
+        .rollerski: "RollerSki",
+        .rowing: "Rowing",
+        .sail: "Sail",
+        .skateboard: "Skateboard",
+        .snowboard: "Snowboard",
+        .snowshoe: "Snowshoe",
+        .soccer: "Soccer",
+        .squash: "Squash",
+        .standuppaddling: "StandUpPaddling",
+        .stairstepper: "StairStepper",
+        .surfing: "Surfing",
+        .tabletennis: "TableTennis",
+        .tennis: "Tennis",
+        .velomobile: "Velomobile",
+        .windsurf: "Windsurf",
+        .wheelchair: "Wheelchair",
+        .workout: "Workout",
+        .yoga: "Yoga",
     ]
     
     var defaultType: SportType {
@@ -34,6 +71,80 @@ enum AppIntentSportType: String, AppEnum {
             return .hike
         case .rowing:
             return .rowing
+        case .alpineski:
+            return .alpineski
+        case .weighttraining:
+            return .weighttraining
+            
+        
+        case .badminton:
+            return .badminton
+        case .backcountryski:
+            return .backcountryski
+        case .canoeing:
+            return .canoeing
+        case .crossfit:
+            return .crossfit
+        case .elliptical:
+            return .elliptical
+        case .golf:
+            return .golf
+        case .iceskate:
+            return .iceskate
+        case .inlineskate:
+            return .inlineskate
+        case .handcycle:
+            return .handcycle
+        case .highintensityintervaltraining:
+            return .highintensityintervaltraining
+        case .kayaking:
+            return .kayaking
+        case .kitesurf:
+            return .kitesurf
+        case .nordicski:
+            return .nordicski
+        case .pickleball:
+            return .pickleball
+        case .pilates:
+            return .pilates
+        case .racquetball:
+            return .racquetball
+        case .rockclimbing:
+            return .rockclimbing
+        case .rollerski:
+            return .rollerski
+        case .sail:
+            return .sail
+        case .skateboard:
+            return .skateboard
+        case .snowboard:
+            return .snowboard
+        case .snowshoe:
+            return .snowshoe
+        case .soccer:
+            return .soccer
+        case .squash:
+            return .squash
+        case .standuppaddling:
+            return .standuppaddling
+        case .stairstepper:
+            return .stairstepper
+        case .surfing:
+            return .surfing
+        case .tabletennis:
+            return .tabletennis
+        case .tennis:
+            return .tennis
+        case .velomobile:
+            return .velomobile
+        case .windsurf:
+            return .windsurf
+        case .wheelchair:
+            return .wheelchair
+        case .workout:
+            return .workout
+        case .yoga:
+            return .yoga
         }
     }
 }
--- Shared/Source/Entity/SportType.swift
+++ Shared/Source/Entity/SportType.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum SportType: String, CaseIterable, Identifiable, Codable {
-    case run, ride, swim, walk, hike, rowing
+    case run, weighttraining, ride, walk, swim, hike, alpineski, badminton, backcountryski, canoeing, crossfit, elliptical, golf, iceskate, inlineskate, handcycle, highintensityintervaltraining, kayaking, kitesurf, nordicski, pickleball, pilates, racquetball, rockclimbing, rollerski, rowing, sail, skateboard, snowboard, snowshoe, soccer, squash, standuppaddling, stairstepper, surfing, tabletennis, tennis, velomobile, windsurf, wheelchair, workout, yoga
     
     var title: String {
         switch self {
@@ -24,9 +24,80 @@ enum SportType: String, CaseIterable, Identifiable, Codable {
             return "Hike"
         case .rowing:
             return "Rowing"
+        case .weighttraining:
+            return "WeightTraining"
+        case .alpineski:
+            return "AlpineSki"
+        case .badminton:
+            return "Badminton"
+        case .backcountryski:
+            return "BackcountrySki"
+        case .canoeing:
+            return "Canoeing"
+        case .crossfit:
+            return "Crossfit"
+        case .elliptical:
+            return "Elliptical"
+        case .golf:
+            return "Golf"
+        case .iceskate:
+            return "IceSkate"
+        case .inlineskate:
+            return "InlineSkate"
+        case .handcycle:
+            return "Handcycle"
+        case .highintensityintervaltraining:
+            return "HighIntensityIntervalTraining"
+        case .kayaking:
+            return "Kayaking"
+        case .kitesurf:
+            return "Kitesurf"
+        case .nordicski:
+            return "NordicSki"
+        case .pickleball:
+            return "Pickleball"
+        case .pilates:
+            return "Pilates"
+        case .racquetball:
+            return "Racquetball"
+        case .rockclimbing:
+            return "RockClimbing"
+        case .rollerski:
+            return "RollerSki"
+        case .sail:
+            return "Sail"
+        case .skateboard:
+            return "Skateboard"
+        case .snowboard:
+            return "Snowboard"
+        case .snowshoe:
+            return "Snowshoe"
+        case .soccer:
+            return "Soccer"
+        case .squash:
+            return "Squash"
+        case .standuppaddling:
+            return "StandUpPaddling"
+        case .stairstepper:
+            return "StairStepper"
+        case .surfing:
+            return "Surfing"
+        case .tabletennis:
+            return "TableTennis"
+        case .tennis:
+            return "Tennis"
+        case .velomobile:
+            return "Velomobile"
+        case .windsurf:
+            return "Windsurf"
+        case .wheelchair:
+            return "Wheelchair"
+        case .workout:
+            return "Workout"
+        case .yoga:
+            return "Yoga"
         }
     }
-    
     var id: String {
         rawValue
     }
